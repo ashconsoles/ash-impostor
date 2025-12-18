43885f9637436ce288abf29025ac1660185370b6
Ash Consoles <ashconsoles@gmail.com>
2025-12-18 21:13:44 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,43 +1,34 @@
-const CACHE_NAME = "ash-impostor-v13";
-
-const FILES_TO_CACHE = [
+const CACHE_NAME = "ash-impostor-v14"; // ⬅ bump version here
+const ASSETS = [
   "./",
   "./index.html",
-  "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjB749IpoWxO9eRIs5uRT0IY1rWNlXHcd_VMztqSQr4G5MsyCRzp0zCuGBJbJSthkXb3NFhzEUljEhTC-PRM1ohfrcOjrYjy9JRG1X8ihWMBsyGvOc_iqEycXg-EPPASuFWziSEE8g0IJrX1t8jqXDOjC_vBTBDFVlnP4_1OBodEJWmCw9gBPThu5lGw-U/s500/1.png"
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./GL1.png",
+  "./qrcode.jpg"
 ];
 
-// INSTALL
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
   self.skipWaiting();
 });
 
-// ACTIVATE
-self.addEventListener("activate", event => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
+      Promise.all(
+        keys.map(key => key !== CACHE_NAME && caches.delete(key))
+      )
     )
   );
   self.clients.claim();
 });
 
-// FETCH (OFFLINE FIX)
-self.addEventListener("fetch", event => {
-  // If this is a page navigation, always serve index.html
-  if (event.request.mode === "navigate") {
-    event.respondWith(
-      caches.match("./index.html").then(response => response)
-    );
-    return;
-  }
-
-  // Otherwise, try cache first, then network
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
